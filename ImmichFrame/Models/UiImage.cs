4f2c47f173652b09d18c95a3db248ec897068645
JW-CH <17313367+JW-CH@users.noreply.github.com>
2024-03-27 16:22:46 +0100

    More cleanup & use single TransitioningContentControl

---
+++ ImmichFrame/Models/UiImage.cs
@@ -0,0 +1,10 @@
+﻿using Avalonia.Media.Imaging;
+
+namespace ImmichFrame.Models
+{
+    public class UiImage
+    {
+        public Bitmap Image { get; set; }
+        public Bitmap ThumbhashImage { get; set; }
+    }
+}
