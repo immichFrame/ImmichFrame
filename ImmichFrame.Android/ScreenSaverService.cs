60556141910ba438e2ba574b15e831fec1bc65f3
3rob3 <robrogers@zoominternet.net>
2024-06-17 11:45:33 -0400

    Add Android screensaver service Add Android TV Leanback launcher support

---
+++ ImmichFrame.Android/ScreenSaverService.cs
@@ -0,0 +1,38 @@
+﻿using Android.App;
+using Android.Content;
+using Android.Content.PM;
+using Android.Service.Dreams;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ImmichFrame.Android
+{
+    [Service(Name = "com.immichframe.immichframe.ScreenSaverService", Permission = "android.permission.BIND_DREAM_SERVICE")]
+    public class ScreenSaverService : DreamService
+    {
+        public override void OnAttachedToWindow()
+        {
+            base.OnAttachedToWindow();
+            base.Fullscreen = true;
+        }
+        public override void OnDreamingStarted()
+        {
+            base.OnDreamingStarted();
+            // Launch main activity when the screensaver starts
+            var intent = new Intent(this, typeof(MainActivity));
+            intent.AddFlags(ActivityFlags.NewTask);
+            StartActivity(intent);
+
+            // Finish the dream (screensaver)
+            Finish();
+        }
+
+        public override void OnDreamingStopped()
+        {
+            base.OnDreamingStopped();
+        }
+    }
+}
