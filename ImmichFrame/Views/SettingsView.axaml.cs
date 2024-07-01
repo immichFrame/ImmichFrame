f9e93eecab0fbe60bc7a2251ed12a9439a1e873b
3rob3 <robrogers@zoominternet.net>
2024-07-01 09:49:04 -0400

    Keep selected setting viewable (not covered by soft keyboard).

--- ImmichFrame/Views/SettingsView.axaml.cs
+++ ImmichFrame/Views/SettingsView.axaml.cs
@@ -1,13 +1,34 @@
+using Avalonia;
 using Avalonia.Controls;
-using ImmichFrame.ViewModels;
+using Avalonia.Input;
+using Avalonia.Interactivity;
 
 namespace ImmichFrame.Views
 {
     public partial class SettingsView : BaseView
     {
+        private ScrollViewer? _scrollViewer;
         public SettingsView()
         {
             InitializeComponent();
+            _scrollViewer = this.FindControl<ScrollViewer>("scrollViewer");
+            var stackPanel = this.FindControl<StackPanel>("stackPanel");
+            if(stackPanel != null )
+            {
+                stackPanel.AddHandler(GotFocusEvent, GotFocus_Handler!, RoutingStrategies.Bubble);
+            }
         }
+        private void GotFocus_Handler(object sender, GotFocusEventArgs e)
+        {
+            if (e.Source is TextBox textBox)
+            {
+                var textBoxPosition = textBox.TranslatePoint(new Point(0, 0), _scrollViewer!);
+                if (textBoxPosition.HasValue)
+                {
+                    var targetOffsetY = _scrollViewer!.Offset.Y + textBoxPosition.Value.Y - 40;
+                    _scrollViewer.Offset = new Vector(0, targetOffsetY);
+                }
+            }
+        }        
     }
 }
