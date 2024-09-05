f8333a8fdc1b465f1186410da9368dd18e58bbc4
JW-CH <17313367+JW-CH@users.noreply.github.com>
2024-09-05 16:33:19 +0200

    first draft of the kiosk

--- immichFrame.Kiosk/src/lib/api.ts
+++
@@ -1,7 +0,0 @@
-import type { paths } from '$lib/immichFrameApi';
-import createClient from 'openapi-fetch';
-
-export const { GET, POST, PATCH, PUT, DELETE, HEAD, TRACE } = createClient<paths>({
-	baseUrl: 'https://localhost:7018'
-	// headers: { authorization: `Bearer ${bearerToken}` }
-});
