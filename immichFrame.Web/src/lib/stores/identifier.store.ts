6b8d3789b9c714ff260ff745421c3fb8ae79b07e
JW-CH <17313367+JW-CH@users.noreply.github.com>
2024-12-11 17:15:50 +0100

    implement local client identifier

---
+++ immichFrame.Web/src/lib/stores/identifier.store.ts
@@ -0,0 +1,25 @@
+
+import { writable } from 'svelte/store';
+
+function persistStore(key: string, defaultValue: string) {
+    const storedValue = localStorage.getItem(key);
+    const initialValue: string = storedValue ? JSON.parse(storedValue) : defaultValue;
+
+    const store = writable(initialValue);
+
+    store.subscribe((value) => {
+        localStorage.setItem(key, JSON.stringify(value));
+    });
+
+    return store;
+}
+
+function generateGUID() {
+    return 'xxxxxxxx-xxxx-4xxx-yxxx-xxxxxxxxxxxx'.replace(/[xy]/g, function (c) {
+        const r = (Math.random() * 16) | 0,
+            v = c === 'x' ? r : (r & 0x3) | 0x8;
+        return v.toString(16);
+    });
+}
+
+export const clientIdentifierStore = persistStore('clientIdentifier', generateGUID());
