30ac289b6adaf9c9132d705f64dd50bba2818f35
Michel Martel <m.martel02@gmail.com>
2024-12-12 16:04:34 -0700

    Added 2 types of text-shadow for small & large text

--- immichFrame.Web/src/lib/components/elements/asset-info.svelte
+++ immichFrame.Web/src/lib/components/elements/asset-info.svelte
@@ -4,34 +4,15 @@
 	import * as locale from 'date-fns/locale';
 	import { configStore } from '$lib/stores/config.store';
 
-	export let asset: AssetResponseDto;
-	export let showLocation: boolean;
-	export let showPhotoDate: boolean;
-	export let showImageDesc: boolean;
-	export let showPeopleDesc: boolean;
+	interface Props {
+		asset: AssetResponseDto;
+		showLocation: boolean;
+		showPhotoDate: boolean;
+		showImageDesc: boolean;
+		showPeopleDesc: boolean;
+	}
 
-	$: assetDate = asset.exifInfo?.dateTimeOriginal;
-	$: desc = asset.exifInfo?.description ?? '';
-
-	$: time = assetDate ? new Date(assetDate) : null;
-
-	const selectedLocale = $configStore.language;
-
-	const localeToUse =
-		(selectedLocale && locale[selectedLocale as keyof typeof locale]) || locale.enUS;
-
-	$: formattedDate = time
-		? format(time, $configStore.photoDateFormat ?? 'dd.MM.yyyy', { locale: localeToUse })
-		: null;
-
-	$: location = formatLocation(
-		$configStore.imageLocationFormat ?? 'City,State,Country',
-		asset.exifInfo?.city ?? '',
-		asset.exifInfo?.state ?? '',
-		asset.exifInfo?.country ?? ''
-	);
-
-	$: availablePeople = asset.people?.filter((x) => x.name);
+	let { asset, showLocation, showPhotoDate, showImageDesc, showPeopleDesc }: Props = $props();
 
 	function formatLocation(format: string, city?: string, state?: string, country?: string) {
 		const locationParts: Array<string> = new Array();
@@ -49,6 +30,27 @@
 
 		return Array.from(locationParts).join(', ');
 	}
+	let assetDate = $derived(asset.exifInfo?.dateTimeOriginal);
+	let desc = $derived(asset.exifInfo?.description ?? '');
+	let time = $derived(assetDate ? new Date(assetDate) : null);
+	const selectedLocale = $configStore.language;
+
+	const localeToUse =
+		(selectedLocale && locale[selectedLocale as keyof typeof locale]) || locale.enUS;
+	let formattedDate = $derived(
+		time
+			? format(time, $configStore.photoDateFormat ?? 'dd.MM.yyyy', { locale: localeToUse })
+			: null
+	);
+	let location = $derived(
+		formatLocation(
+			$configStore.imageLocationFormat ?? 'City,State,Country',
+			asset.exifInfo?.city ?? '',
+			asset.exifInfo?.state ?? '',
+			asset.exifInfo?.country ?? ''
+		)
+	);
+	let availablePeople = $derived(asset.people?.filter((x) => x.name));
 </script>
 
 {#if showPhotoDate || showPhotoDate || showImageDesc || showPeopleDesc}
@@ -59,18 +61,18 @@
 		{$configStore.style == 'blur' ? 'backdrop-blur-lg rounded-tl-2xl' : ''}	"
 	>
 		{#if showPhotoDate && formattedDate}
-			<p class="text-sm font-thin text-shadow-custom">{formattedDate}</p>
+			<p class="text-sm font-thin text-shadow-sm">{formattedDate}</p>
 		{/if}
 		{#if showImageDesc && desc}
-			<p class="text-base font-light text-shadow-custom">{desc}</p>
+			<p class="text-base font-light text-shadow-sm">{desc}</p>
 		{/if}
 		{#if showPeopleDesc && availablePeople}
-			<p class="text-sm font-light text-shadow-custom">
+			<p class="text-sm font-light text-shadow-sm">
 				{availablePeople.map((x) => x.name).join(', ')}
 			</p>
 		{/if}
 		{#if showLocation && location}
-			<p class="text-base font-light text-shadow-custom">{location}</p>
+			<p class="text-base font-light text-shadow-sm">{location}</p>
 		{/if}
 	</div>
 {/if}
