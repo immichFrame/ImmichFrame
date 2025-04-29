96ea28e631f3a637cd0e87424867985effe83fb4
JW-CH <17313367+JW-CH@users.noreply.github.com>
2025-04-29 09:53:51 +0200

    update caches for other services

--- ImmichFrame.Core/Services/IcalCalendarService.cs
+++ ImmichFrame.Core/Services/IcalCalendarService.cs
@@ -5,6 +5,7 @@ using ImmichFrame.WebApi.Helpers;
 public class IcalCalendarService : ICalendarService
 {
     private readonly IServerSettings _serverSettings;
+    private readonly ApiCache<List<IAppointment>> _appointmentCache = new(TimeSpan.FromMinutes(15));
 
     public IcalCalendarService(IServerSettings serverSettings)
     {
@@ -13,28 +14,25 @@ public class IcalCalendarService : ICalendarService
 
     public async Task<List<IAppointment>> GetAppointments()
     {
-        var appointments = new List<IAppointment>();
-
-        var icals = await GetCalendars(_serverSettings.Webcalendars);
-
-        foreach (var ical in icals)
+        return await _appointmentCache.GetOrAddAsync("appointments", async () =>
         {
-            var calendar = Calendar.Load(ical);
+            var appointments = new List<IAppointment>();
 
-            appointments.AddRange(calendar.GetOccurrences(DateTime.UtcNow, DateTime.Today.AddDays(1)).Select(x => x.ToAppointment()));
-        }
+            var icals = await GetCalendars(_serverSettings.Webcalendars);
 
-        return appointments;
+            foreach (var ical in icals)
+            {
+                var calendar = Calendar.Load(ical);
+
+                appointments.AddRange(calendar.GetOccurrences(DateTime.UtcNow, DateTime.Today.AddDays(1)).Select(x => x.ToAppointment()));
+            }
+
+            return appointments;
+        });
     }
 
-    private (DateTime fetchDate, List<string> calendars)? lastCalendars;
     public async Task<List<string>> GetCalendars(IEnumerable<string> calendars)
     {
-        if (lastCalendars != null && lastCalendars.Value.fetchDate.AddMinutes(15) > DateTime.Now)
-        {
-            return lastCalendars.Value.calendars;
-        }
-
         var icals = new List<string>();
 
         foreach (var webcal in calendars)
@@ -54,8 +52,6 @@ public class IcalCalendarService : ICalendarService
                 }
             }
         }
-
-        lastCalendars = (DateTime.Now, icals);
 
         return icals;
     }
--- ImmichFrame.Core/Services/OpenWeatherMapService.cs
+++ ImmichFrame.Core/Services/OpenWeatherMapService.cs
@@ -4,30 +4,25 @@ using ImmichFrame.Core.Interfaces;
 public class OpenWeatherMapService : IWeatherService
 {
     private readonly IServerSettings _settings;
+    private readonly ApiCache<IWeather?> _weatherCache = new(TimeSpan.FromMinutes(5));
     public OpenWeatherMapService(IServerSettings settings)
     {
         _settings = settings;
     }
 
-    private (DateTime fetchDate, IWeather? weather)? lastWeather;
     public async Task<IWeather?> GetWeather()
     {
-        // Check if cached weather data is still valid
-        if (lastWeather.HasValue && lastWeather.Value.weather != null && lastWeather.Value.fetchDate.AddMinutes(5) > DateTime.Now)
+        return await _weatherCache.GetOrAddAsync("weather", async () =>
         {
-            return lastWeather.Value.weather;
-        }
+            var weatherLatLong = _settings.WeatherLatLong;
 
-        var weatherLatLong = _settings.WeatherLatLong;
+            var weatherLat = !string.IsNullOrWhiteSpace(weatherLatLong) ? float.Parse(weatherLatLong!.Split(',')[0]) : 0f;
+            var weatherLong = !string.IsNullOrWhiteSpace(weatherLatLong) ? float.Parse(weatherLatLong!.Split(',')[1]) : 0f;
 
-        var weatherLat = !string.IsNullOrWhiteSpace(weatherLatLong) ? float.Parse(weatherLatLong!.Split(',')[0]) : 0f;
-        var weatherLong = !string.IsNullOrWhiteSpace(weatherLatLong) ? float.Parse(weatherLatLong!.Split(',')[1]) : 0f;
+            var weather = await GetWeather(weatherLat, weatherLong);
 
-        var weather = await GetWeather(weatherLat, weatherLong);
-
-        lastWeather = (DateTime.Now, weather);
-
-        return weather;
+            return weather;
+        });
     }
 
     public async Task<IWeather?> GetWeather(double latitude, double longitude)
