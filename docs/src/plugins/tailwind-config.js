711440a84834ee95891507704520ea7d67f68ec4
JW-CH <17313367+JW-CH@users.noreply.github.com>
2025-06-04 16:13:23 +0200

    immich style landing page

---
+++ docs/src/plugins/tailwind-config.js
@@ -0,0 +1,9 @@
+module.exports = function tailwindPlugin(context, options) {
+    return {
+        name: "tailwind-plugin",
+        configurePostCss(postcssOptions) {
+            postcssOptions.plugins = [require("@tailwindcss/postcss")];
+            return postcssOptions;
+        },
+    };
+};
