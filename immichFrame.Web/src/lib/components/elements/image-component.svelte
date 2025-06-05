1fef35f32143d7dd6500bec30a6fc46661429135
pete1450 <adampete06@gmail.com>
2025-06-04 20:51:05 -0500

    remove whitespace change

--- immichFrame.Web/src/lib/components/elements/image-component.svelte
+++ immichFrame.Web/src/lib/components/elements/image-component.svelte
@@ -77,38 +77,39 @@
 			transition:blur={{ duration: transitionDuration }}
 		>
 			{#if split}
-				<div class="grid grid-cols-2">				<div id="image_portrait_1" class="relative grid border-r-2 border-primary h-dvh-safe">
-					<Image
-						multi={true}
-						image={images[0]}
-						{interval}
-						{showLocation}
-						{showPhotoDate}
-						{showImageDesc}
-						{showPeopleDesc}
-						{showAlbumName}
-						{imageFill}
-						{imageZoom}
-						{imagePan}
-						bind:showInfo
-					/>
-				</div>
-				<div id="image_portrait_2" class="relative grid border-l-2 border-primary h-dvh-safe">
-					<Image
-						multi={true}
-						image={images[1]}
-						{interval}
-						{showLocation}
-						{showPhotoDate}
-						{showImageDesc}
-						{showPeopleDesc}
-						{showAlbumName}
-						{imageFill}
-						{imageZoom}
-						{imagePan}
-						bind:showInfo
-					/>
-				</div>
+				<div class="grid grid-cols-2">
+					<div id="image_portrait_1" class="relative grid border-r-2 border-primary h-dvh-safe">
+						<Image
+							multi={true}
+							image={images[0]}
+							{interval}
+							{showLocation}
+							{showPhotoDate}
+							{showImageDesc}
+							{showPeopleDesc}
+							{showAlbumName}
+							{imageFill}
+							{imageZoom}
+							{imagePan}
+							bind:showInfo
+						/>
+					</div>
+					<div id="image_portrait_2" class="relative grid border-l-2 border-primary h-dvh-safe">
+						<Image
+							multi={true}
+							image={images[1]}
+							{interval}
+							{showLocation}
+							{showPhotoDate}
+							{showImageDesc}
+							{showPeopleDesc}
+							{showAlbumName}
+							{imageFill}
+							{imageZoom}
+							{imagePan}
+							bind:showInfo
+						/>
+					</div>
 				</div>
 			{:else}
 				<div id="image_default" class="relative grid h-dvh-safe w-screen">
