2df6f4f8e940d5f768885a5dc2430d03c0b1c9e4
JW-CH <17313367+JW-CH@users.noreply.github.com>
2025-06-06 08:51:50 +0200

    remove docusaurus social card and other icons, add own social-card

--- docs/docusaurus.config.ts
+++ docs/docusaurus.config.ts
@@ -66,7 +66,7 @@ const config: Config = {
 
   themeConfig: {
     // Replace with your project's social card
-    image: 'img/docusaurus-social-card.jpg',
+    image: 'img/social-card.jpg',
     navbar: {
       title: 'ImmichFrame',
       logo: {
