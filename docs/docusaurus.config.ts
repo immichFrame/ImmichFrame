b4bc790054792e2a15c699d3282d3dc486b9c8f2
JW-CH <17313367+JW-CH@users.noreply.github.com>
2025-06-06 09:01:13 +0200

    change social card file extension

--- docs/docusaurus.config.ts
+++ docs/docusaurus.config.ts
@@ -66,7 +66,7 @@ const config: Config = {
 
   themeConfig: {
     // Replace with your project's social card
-    image: 'img/social-card.jpg',
+    image: 'img/social-card.png',
     navbar: {
       title: 'ImmichFrame',
       logo: {
