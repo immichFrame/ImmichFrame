977c8d29a5879d903a64544d32257fc7c35bef1d
JW-CH <17313367+JW-CH@users.noreply.github.com>
2025-10-09 22:54:02 +0200

    make api more robust

--- ImmichFrame.Core/Api/ImmichApi.cs
+++ ImmichFrame.Core/Api/ImmichApi.cs
@@ -4,7 +4,7 @@
     {
         public ImmichApi(string url, System.Net.Http.HttpClient httpClient)
         {
-            _baseUrl = url + _baseUrl;
+            _baseUrl = $"{url.TrimEnd('/')}/{_baseUrl.TrimStart('/')}";
             _httpClient = httpClient;
             _settings = new System.Lazy<Newtonsoft.Json.JsonSerializerSettings>(CreateSerializerSettings);
         }
